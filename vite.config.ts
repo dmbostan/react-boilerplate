c60e25aa0be91c197d965d3ee48283a4bfd92fbd
Dmitri Bostan <dm.bostan@gmail.com>
2023-10-19 15:22:03 +0200

    WIP: Adjust path aliases based on the new folder structure

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,18 @@ import * as path from "path";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [
+        { find: '@atoms', replacement: path.resolve(__dirname, 'src/components/atoms') },
+        { find: '@molecules', replacement: path.resolve(__dirname, 'src/components/molecules') },
+        { find: '@organisms', replacement: path.resolve(__dirname, 'src/components/organisms') },
+        { find: '@templates', replacement: path.resolve(__dirname, 'src/components/templates') },
+        { find: '@pages', replacement: path.resolve(__dirname, 'src/components/pages') },
+        { find: '@hooks', replacement: path.resolve(__dirname, 'src/components/hooks') },
+        { find: '@context', replacement: path.resolve(__dirname, 'src/components/context') },
+        { find: '@assets', replacement: path.resolve(__dirname, 'src/components/assets') },
+        { find: '@styles', replacement: path.resolve(__dirname, 'src/components/styles') },
+        { find: '@utils', replacement: path.resolve(__dirname, 'src/components/utils') },
+    ],
   },
   test: {
     environment: 'jsdom',
