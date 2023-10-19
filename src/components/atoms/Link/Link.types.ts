6bf5adf4f4c0b336fa5a9c020436365ab7131813
Dmitri Bostan <dm.bostan@gmail.com>
2023-10-19 15:50:16 +0200

    WIP: Add support for rel in Link component

--- src/components/atoms/Link/Link.types.ts
+++ src/components/atoms/Link/Link.types.ts
@@ -1,6 +1,7 @@
 import {ReactNode} from "react";
 
 export type LinkTypes = {
+    rel?: string,
     target?: '_blank' | '_self' | '_parent' | '_top',
     href: string,
     children: string | ReactNode | ReactNode[] | null
