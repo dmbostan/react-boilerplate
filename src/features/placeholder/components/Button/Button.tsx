9d5f06616fbe14e79d65aae5a83d425430886de7
Dmitri Bostan <dm.bostan@gmail.com>
2023-11-03 22:08:53 +0100

    REFACTOR: Cleanup codebase

--- src/features/placeholder/components/Button/Button.tsx
+++ src/features/placeholder/components/Button/Button.tsx
@@ -1,8 +1,12 @@
 import { ButtonTypes } from './Button.types.ts';
+import styles from './Button.module.css';
+import classNamesHandler from '@shared/utils/classNamesHandler.ts';
 
 const Button = ({ clickHandler, className, children }: ButtonTypes) => {
+  const classNames = classNamesHandler(className, styles.button);
+
   return (
-    <button onClick={clickHandler} className={className}>
+    <button onClick={clickHandler} className={classNames}>
       {children}
     </button>
   );
